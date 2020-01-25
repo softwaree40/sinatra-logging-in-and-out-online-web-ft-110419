122d8d994237c7bd2d7c5ce0e9644211e251e73f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-25 23:06:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,13 +16,13 @@ class ApplicationController < Sinatra::Base
     
     if @user 
       session[:user_id] = @user.id
-      redirect "/account/#{@user.id}"
+      redirect "/account"
     else
       erb :error
     end
   end
 
-  get '/account/:id' do
+  get '/account' do
     @user = User.find_by(id: session[:user_id])
     if @user
       erb :account
