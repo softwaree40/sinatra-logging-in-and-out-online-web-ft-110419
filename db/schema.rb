62322b50747621d6edd1c1c5ff534a1faa364179
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-25 23:10:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200121211546) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.integer "balance"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
